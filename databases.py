78e181758769829425a245c5101e492ec2db5e31
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:43:36 +0200

    databases

--- databases.py
+++ databases.py
@@ -8,18 +8,24 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Write your functions to interact with the database here :
-
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def create_product(product, description, year, price):
+  new_product = Product(
+    product=product,
+    description=description,
+    year=year,
+    price=price)
+  session.add(new_product)
+  session.commit()
 
 def update_product():
   #TODO: complete the functions (you will need to change the function's inputs)
   pass
 
 def delete_product(id):
-  pass
+  session.query(Product).filter_by(
+    id= id).delete()
+  session.commit()
+
 
 def get_product(id):
   pass
